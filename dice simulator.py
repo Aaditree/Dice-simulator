c2ddc3519547ca3ea8c92045305d7d80f4e0bc3e
Aaditree <43510617+Aaditree@users.noreply.github.com>
2020-05-31 20:16:13 +0530

    simple use of randInt

---
+++ dice simulator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 20:02:55 2020
+
+@author: Aaditree Jaisswal
+"""
+
+print("[--------]")
+print("|        |")
+
+import random 
+n = random.randint(1,6)
+
+print("|   ",n,"  |")
+print("|        |")
+print("[--------]")
